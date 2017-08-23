3d1d5678fc0167e2db1c96e37e14d66991de66a3
Mattycodes <matt.nick.fagioli@gmail.com>
2017-08-23 14:18:14 -0400

    Fixes and angular validation bug and allows the user to view password inputs.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,18 @@
+$(document).ready(function() {
+  passwordReveal();
+  passwordHide();
+});
 
+function passwordReveal() {
+  $(document).on('click', 'span.glyphicon-eye-open', function() {
+    $(this).parent().parent().children('input.password-input').attr('type', 'text');
+    $(this).replaceWith("<span class='glyphicon glyphicon-eye-close'></span>");
+  });
+};
+
+function passwordHide() {
+  $(document).on('click', 'span.glyphicon-eye-close', function() {
+    $(this).parent().parent().children('input.password-input').attr('type', 'password');
+    $(this).replaceWith("<span class='glyphicon glyphicon-eye-open'></span>");
+  });
+};
